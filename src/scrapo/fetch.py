9da965d498c611f77eccb6dc5ec1bf91c57597b1
feboss <feboss86@gmail.com>
2022-07-25 03:43:27 +0200

    fix logging

--- src/scrapo/fetch.py
+++ src/scrapo/fetch.py
@@ -1,28 +1,30 @@
 import asyncio
-import logging
+from logging import getLogger
 import random
 import aiohttp
+
+LOG = getLogger(__name__)
 
 
 async def get(session, url, retries=4, backoff_in_seconds=2, pow=2, params=None, headers=None, sleep=0):
 
     try:
-        logging.getLogger('aiohttp.client').debug(
-            f'Request starting at <{url}>')
+        LOG.debug(f'Request starting at <{url}>')
         elapsed = asyncio.get_event_loop().time()
         async with session.get(url, data=params, headers=headers) as response:
             elapsed = asyncio.get_event_loop().time() - elapsed
-            logging.getLogger('aiohttp.client').debug(
-                "Request ended in {} - URL= {} - status: {}".format(elapsed, response.url, response.status))
+            LOG.debug("Request ended in {} - URL= {} - status: {}".format(elapsed,
+                      response.url, response.status))
 
             r = await response.text()
+
             # fix for "Response payload is not completed"
             await asyncio.sleep(0)
+
             return r
 
     except aiohttp.ClientResponseError as e:
-        logging.getLogger('aiohttp.client').warning(
-            f"Request error: {e} - Retry")
+        LOG.warning(f"Request error: {e} - Retry")
         # BACKOFF
         if retries > 0:
             sleep = (backoff_in_seconds * 2 ** pow +
@@ -30,7 +32,7 @@ async def get(session, url, retries=4, backoff_in_seconds=2, pow=2, params=None,
             await asyncio.sleep(sleep)
             await get(session, url, retries=retries-1, pow=pow+1, params=params)
     except Exception as e:
-        print(e)
+        LOG.error(e)
 
 
 async def get_all(session, urls, params=None, headers={}, sleep=0):
